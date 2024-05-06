45972195d7c550ad5fd7d6e33a9a583cebfbbe73
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-07 00:04:43 +0530

    stopmarket.js

---
+++ stopmarket.js
@@ -0,0 +1,40 @@
+
+function nifty(i) {
+  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
+let fyersend="&resolution=5&theme=light";
+  let groww="https://groww.in/charts/options/nifty/"+i.slice(4,100); 
+  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
+let result = i.slice(-5);
+  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
+let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
+let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
+  return a+b+c;
+  }
+
+
+function banknifty(i) {
+  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
+let fyersend="&resolution=5&theme=light";
+  let groww="https://groww.in/charts/options/banknifty/"+i.slice(4,100); 
+  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
+let result = i.slice(-5);
+  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
+let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
+let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
+  return a+b+c;
+  }
+
+function finnifty(i) {
+  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
+let fyersend="&resolution=5&theme=light";
+  let groww="https://groww.in/charts/options/finnifty/"+i.slice(4,100); 
+  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
+let result = i.slice(-5);
+  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
+let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
+let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
+  return a+b+c;
+  }
+
+  
+  
