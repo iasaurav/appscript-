8fe4673b120c73adc0ab32c84112b17f92de7048
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-09 13:27:41 +0530

    stopmarket.js

--- stopmarket.js
+++ stopmarket.js
@@ -1,33 +1,8 @@
 
-function nifty(i) {
+function nse(i,j) {
   let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
 let fyersend="&resolution=5&theme=light";
-  let groww="https://groww.in/charts/options/nifty/"+i.slice(4,100); 
-  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
-let result = i.slice(-5);
-  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
-let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
-let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
-  return a+b+c;
-  }
-
-
-function banknifty(i) {
-  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
-let fyersend="&resolution=5&theme=light";
-  let groww="https://groww.in/charts/options/banknifty/"+i.slice(4,100); 
-  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
-let result = i.slice(-5);
-  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
-let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
-let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
-  return a+b+c;
-  }
-
-function finnifty(i) {
-  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
-let fyersend="&resolution=5&theme=light";
-  let groww="https://groww.in/charts/options/finnifty/"+i.slice(4,100); 
+  let groww="https://groww.in/charts/options/"+j+i.slice(4,100); 
   let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
 let result = i.slice(-5);
   let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
@@ -38,10 +13,14 @@ let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td
 
 
 
-function sensex(i) {
+
+
+
+
+function bse(i) {
   let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
 let fyersend="&resolution=5&theme=light";
-  let groww="https://groww.in/charts/options/sp-bse-sensex/"+i.slice(4,100); 
+  let groww="https://groww.in/charts/options/sp-bse-"+i.slice(4,100); 
   let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
 let result = i.slice(-5);
   let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
@@ -50,17 +29,7 @@ let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td
   return a+b+c;
   }
 
-function bankex(i) {
-  let fyers="https://trade.fyers.in/popout/index.html?symbol="; 
-let fyersend="&resolution=5&theme=light";
-  let groww="https://groww.in/charts/options/sp-bse-bankex/"+i.slice(4,100); 
-  let sensibull="https://web.sensibull.com/chart?tradingSymbol="; 
-let result = i.slice(-5);
-  let a="<tr><td><a href='"+groww+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+groww+"PE'target='_blank'>"+result+"</a></td>";
-let b="<td><a href='"+sensibull+i+"CE'target='_blank'>"+result+"</a></td>"+"<td><a href='"+sensibull+i+"'target='_blank'>"+result+"</a></td>";
-let c="<td><a href='"+fyers+i+"CE"+fyersend+"'target='_blank'>"+result+"</a></td>"+"<td><a href='"+i+"PE"+fyersend+"'target='_blank'>"+result+"</a></td></tr>";
-  return a+b+c;
-  }
+
 
 
 
