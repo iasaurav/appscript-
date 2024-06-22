cd5c4541c09cb791ecacdcd2997c0c426aeccf79
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-06-22 11:03:18 +0530

    googlesheet.js

--- googlesheet.js
+++ googlesheet.js
@@ -268,10 +268,10 @@ function generateNumberList(a,b,c,p) {
 function trade(h,i,j,k,l,m,n,o) {
 let tradingview='https://in.tradingview.com/chart/?symbol=';
 let fyers='https://trade.fyers.in/popout/index.html?symbol=';
-  let a="<tr><td><a href='"+tradingview+h+"'>"+h+"</a></td>"+"<td><a href='"+tradingview+i+"'>"+i+"</a></td>";
-let b="<td><a href='"+tradingview+j+"'>"+j+"</a></td>"+"<td><a href='"+tradingview+k+"'>"+k+"</a></td>";
-let c="<td><a href='"+tradingview+l+"'>"+l+"</a></td>"+"<td><a href='"+tradingview+m+"'>"+m+"</a></td>";
-let d="<td><a href='"+fyers+n+"&resolution=5&theme=light'>"+n+"</a></td>"+"<td><a href='"+fyers+o+"&resolution=5&theme=light'>"+o+"</a></td></tr>";
+let a="<tr><td><a href='"+tradingview+h+"'>NIFTY "+h.slice(16)+"CE</a></td>"+"<td><a href='"+tradingview+i+"'>NIFTY "+i.slice(16)+"PE</a></td>";
+let b="<td><a href='"+tradingview+j+"'>BANKNIFTY "+j.slice(20)+"CE</a></td>"+"<td><a href='"+tradingview+k+"'>BANKNIFTY "+k.slice(20)+"PE</a></td>";
+let c="<td><a href='"+tradingview+l+"'>FINNIFTY "+l.slice(19)+"CE</a></td>"+"<td><a href='"+tradingview+m+"'>FINNIFTY "+m.slice(19)+"PE</a></td>";
+let d="<td><a href='"+fyers+n+"&resolution=5&theme=light'>"+n.slice(17)+"</a></td>"+"<td><a href='"+fyers+o+"&resolution=5&theme=light'>"+o.slice(17)+"PE</a></td></tr>";
   return a+b+c+d;
   }
 
