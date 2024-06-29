2d7c6e56a627fb70e7265b29a915928949f0051c
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-06-29 10:34:35 +0530

    Saurav.js

---
+++ Saurav.js
@@ -0,0 +1,13 @@
+function trade(h,i,j,k,l,m,n,o,p,q,r,s) {
+let tradingview='https://in.tradingview.com/chart/?symbol=';
+let fyers='https://trade.fyers.in/popout/index.html?symbol=';
+let upstox='https://tv.upstox.com/charts/MCX_FO|';
+let ope = "'target='_blank";
+let a="<tr><td><a href='"+tradingview+h+ope+"'>NIFTY "+h.slice(16)+"CE</a></td>"+"<td><a href='"+tradingview+i+ope+"'>NIFTY "+i.slice(16)+"PE</a></td>";
+let b="<td><a href='"+tradingview+j+ope+"'>BANKNIFTY "+j.slice(20)+"CE</a></td>"+"<td><a href='"+tradingview+k+ope+"'>BANKNIFTY "+k.slice(20)+"PE</a></td>";
+let c="<td><a href='"+tradingview+l+ope+"'>FINNIFTY "+l.slice(19)+"CE</a></td>"+"<td><a href='"+tradingview+m+ope+"'>FINNIFTY "+m.slice(19)+"PE</a></td>";
+  let d="<td><a href='"+upstox+p+"?isPopup=true'target='_blank'>"+q+"</a></td>"+"<td><a href='"+upstox+r+"?isPopup=true'target='_blank'>"+s+"</a></td></tr>";
+  return a+b+c+d;
+  }
+
+
