6cb57f8b0b64040e6b5c518e00e3825819e10c6b
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-07-04 23:49:44 +0530

    Saurav.js

--- Saurav.js
+++ Saurav.js
@@ -11,3 +11,18 @@ let c="<td><a href='"+tradingview+l.slice(4,1000)+ope+"'>FINNIFTY "+l.slice(19)+
   }
 
 
+function uptrade(h,i,j,k,l,m) {
+let tradingview='https://in.tradingview.com/symbols/NSE-';
+let fyers='https://trade.fyers.in/popout/index.html?symbol=';
+let upstox='https://tv.upstox.com/charts/MCX_FO|';
+let ope = "'target='_blank";
+let a="<tr><td><a href='"+tradingview+h.slice(4,1000)+ope+"'>NIFTY "+h.slice(16)+"CE</a></td>"+"<td><a href='"+tradingview+i.slice(4,1000)+ope+"'>NIFTY "+i.slice(16)+"PE</a></td>";
+let b="<td><a href='"+tradingview+j.slice(4,1000)+ope+"'>BANKNIFTY "+j.slice(20)+"CE</a></td>"+"<td><a href='"+tradingview+k.slice(4,1000)+ope+"'>BANKNIFTY "+k.slice(20)+"PE</a></td>";
+let c="<td><a href='"+tradingview+l.slice(4,1000)+ope+"'>FINNIFTY "+l.slice(19)+"CE</a></td>"+"<td><a href='"+tradingview+m.slice(4,1000)+ope+"'>FINNIFTY "+m.slice(19)+"PE</a></td>";
+  return a+b+c;
+  }
+
+
+
+
+
